11ffc9009c1732e6a6f9690eb4fd7c2d67086aae
Naysha27XRD <rahmawatinaysha@gmail.com>
2023-01-20 09:12:39 +0700

    first push

---
+++ footer.php
@@ -0,0 +1,24 @@
+    <!-- bagian footer -->
+    <footer>
+        <div class="container text-center">
+            Copyright &copy; 2023 - <?= $d->nama ?>.
+        </div>
+    </footer>
+
+    <script>
+
+       var mobileMenu = document.getElementById("mobileMenu")
+
+       function showMobileMenu(){
+        mobileMenu.style.display = "block"
+       }
+
+       function hideMobileMenu(){
+        mobileMenu.style.display = "none"
+       }
+
+    </script>
+
+    
+</body>
+</html>
